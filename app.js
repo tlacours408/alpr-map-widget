4ea4ffc9d381633f48fc2306aa812c0d08878760
tlacours408 <tlacours@umn.edu>
2026-02-16 13:01:10 -0600

    Change map initial zoom level from 11 to 8
    Signed-off-by: tlacours408 <tlacours@umn.edu>

--- app.js
+++ app.js
@@ -220,7 +220,7 @@ function initMap(){
 	mapObj = L.map('map', {
 		worldCopyJump: true,
 		zoomControl: false
-	}).setView([46.3, -94.2], 11);
+	}).setView([46.3, -94.2], 8);
 
 	
 	//	default zoom controls overlap left-side UI, so reposition them
